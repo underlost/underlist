659b298724e44f9eee5632970981d651dc5d591f
underlost <underlost@gmail.com>
2013-08-23 15:45:09 -0700

    blah

--- underlist/backlog/urls.py
+++ underlist/backlog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import *
 from . import views
 
 urlpatterns = patterns('',
-				
+	url(r'^$', views.index, name = 'index'),			
 	url(r'^add/$', views.add_game, name = 'add-game'),
 	url(r'^edit/(?P<game_guid>\w+)/$', views.edit_game, name = 'edit-game' ),
 	url(r'^delete/(?P<game_guid>\w+)/$', views.delete_game, name = 'delete-game'),
