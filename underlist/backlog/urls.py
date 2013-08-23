0398a8806b8953643edc2a81c1f2dc15ba3ac3db
underlost <underlost@gmail.com>
2013-08-23 14:24:49 -0700

    meh

--- underlist/backlog/urls.py
+++ underlist/backlog/urls.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
-from django.conf.urls.defaults import *
-from django.views.generic.simple import direct_to_template
+from django.conf.urls import *
 from . import views
 
 urlpatterns = patterns('',
