b52f22e5e1ab2252414c00969859c446c25ac776
underlost <underlost@gmail.com>
2013-08-24 11:19:13 -0700

    blah

--- underlist/backlog/admin.py
+++ underlist/backlog/admin.py
@@ -4,6 +4,6 @@ from django.utils.translation import ugettext_lazy as _
 
 class GameAdmin(admin.ModelAdmin):
 	list_display = ('game','added')
-	list_filter = ('is_played', 'is_owned', 'is_complete')
+	list_filter = ('is_owned', 'status')
 	
 admin.site.register(Game, GameAdmin)
